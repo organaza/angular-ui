dd9c7dca47ffe3ba478656dd4795989325f071fa
Stanislav <sokolov.stas@gmail.com>
2018-07-05 10:09:19 +0500

    Add new format

--- src/lib/pipes/duration-to-string.pipe.ts
+++ src/lib/pipes/duration-to-string.pipe.ts
@@ -43,6 +43,12 @@ export class DurationToStringPipe implements PipeTransform {
       if (format === 'hoursOnlyNumber') {
         return Math.floor(value.asHours());
       }
+      if (format === 'hh:mm:ss') {
+        return Math.floor(value.hours()).toString().padStart(2, '0') + ':' + Math.floor(value.minutes()).toString().padStart(2, '0') + ':' + Math.floor(value.seconds()).toString().padStart(2, '0');
+      }
+      if (format === 'hh:mm') {
+        return Math.floor(value.hours()).toString().padStart(2, '0') + ':' + Math.floor(value.minutes()).toString().padStart(2, '0');
+      }
       if (format === 'minutes') {
         return moment.localeData().relativeTime(Math.floor(value.asMinutes()), true, 'mm', false);
       }
