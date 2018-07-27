001ea2b2d27f56a8a2caac9df82b4f8e94ece7d2
Stanislav <sokolov.stas@gmail.com>
2018-07-27 18:05:39 +0500

    Fix public api

--- src/public_api.ts
+++ src/public_api.ts
@@ -34,7 +34,6 @@ export { RangeComponent } from './lib/range/range.component';
 export { SwitchComponent } from './lib/switch/switch.component';
 export { TextinputComponent } from './lib/textinput/textinput.component';
 export { TimePickerComponent } from './lib/timepicker/timepicker.component';
-export { VirtualScrollComponent } from './lib/virtual-scroll/virtual-scroll.component';
 
 // export { DragulaDelayLiftDirective } from './lib/dragula-delay-lift/dragula-delay-lift.directive';
 // export { ModalContainerDirective } from './lib/modal/modal-container.directive';
